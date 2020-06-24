a19b2e4ef83f5e41c2a19d8d4801a70048337b2c
Max Shapiro <magicmax33@gmail.com>
2020-06-24 16:24:48 -0400

    Added Jaeger and Opossum (#4)
    * Added Jaeger and Opossum
    
    * Made PR changes
    
    * Updated routes
    
    * Updated health responses

--- src/routes/{{.Route}}.js
+++ src/routes/{{.Route}}.js
@@ -1,8 +1,16 @@
 import { Router } from "express";
-import { getData } from "../services/dataHandler";
-import ExampleError from "../errors/ExampleError";
+import { getData } from "./../services/dataHandler";
+import ExampleError from "./../errors/ExampleError";
+import Jaeger from "./../jaeger";
+import CircuitBreaker from "opossum";
 
 const router = Router();
+
+const opossumOptions = {
+  timeout: 15000, // If our function takes longer than 10 seconds, trigger a failure
+  errorThresholdPercentage: 50, // When 50% of requests fail, trip the circuit
+  resetTimeout: 30000, // After 30 seconds, try again.
+};
 
 /**
  * GET /api/v1/{{.Route}}
@@ -11,8 +19,10 @@ const router = Router();
  * @response 400 - Error
  */
 router.get("/", async (req, res, next) => {
+  const context = new Jaeger({{.Route}}, req, res);
   try {
-    const data = await getData();
+    const breaker = new CircuitBreaker(getData, opossumOptions);
+    const data = await breaker.fire(context);
     return res.json(data);
   } catch (e) {
     if (e instanceof ExampleError) {
--- src/services/dataHandler.js
+++ src/services/dataHandler.js
@@ -1,6 +1,8 @@
 import ExampleError from "./../errors/ExampleError";
 
-export async function getData() {
+export async function getData(context) {
+  context.start("getData");
+  context.stop();
   throw new ExampleError("TODO: Implement me!");
 }
 
